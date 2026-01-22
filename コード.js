afdfa3841f8726455050d272dc146d791f9ba62a
true-nosuke <nosuke9002@icloud.com>
2026-01-22 11:17:56 +0900

    first commit

---
+++ コード.js
@@ -0,0 +1,8 @@
+// B列をC列に翻訳する関数
+function TranslateEngToJpn(){
+    const sheet = getActiveSheet(); // アクティブなシートを取得
+    const engRow = getValue(sheet, 2); // B列の値を取得
+    const jpnRow = []; // C列に格納する配列
+
+
+}
